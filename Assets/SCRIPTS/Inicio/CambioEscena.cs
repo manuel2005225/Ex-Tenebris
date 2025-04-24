2a4de76c1fd001a25bb95b2dc902dbce90c535ea
Andrejulian21 <julian21arteaga@gmail.com>
2025-04-24 10:05:50 -0500

    pantalla de inicio
    ndaiud

---
+++ Assets/SCRIPTS/Inicio/CambioEscena.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioEscena : MonoBehaviour
+{
+    public void IrAJuego()
+    {
+        SceneManager.LoadScene("Iglesia");
+    }
+}
+
+
+
