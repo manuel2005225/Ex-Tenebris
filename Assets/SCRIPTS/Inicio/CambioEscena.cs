826c18cdb79d556fe2f1e3770e733ee6dae67d83
Askar2231 <148814314+Askar2231@users.noreply.github.com>
2025-04-24 15:25:51 -0500

    asd

--- Assets/SCRIPTS/Inicio/CambioEscena.cs
+++ Assets/SCRIPTS/Inicio/CambioEscena.cs
@@ -1,11 +1,36 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
 public class CambioEscena : MonoBehaviour
 {
+    public Image fadeImage; // Asigna el objeto FadeImage desde el Inspector
+    public float fadeDuration = 1f;
+
     public void IrAJuego()
     {
-        SceneManager.LoadScene("Iglesia");
+        StartCoroutine(FadeOutAndLoadScene("Iglesia"));
+    }
+
+    IEnumerator FadeOutAndLoadScene(string sceneName)
+    {
+        // Asegúrate de que la imagen esté encima y activa
+        fadeImage.gameObject.SetActive(true);
+
+        Color color = fadeImage.color;
+        float t = 0f;
+
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            float alpha = Mathf.Clamp01(t / fadeDuration);
+            fadeImage.color = new Color(color.r, color.g, color.b, alpha);
+            yield return null;
+        }
+
+        // Cargar escena después del fade
+        SceneManager.LoadScene(sceneName);
     }
 }
 
