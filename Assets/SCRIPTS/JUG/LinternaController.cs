1ac1c25d5548b5d656a392e69fbcc699caba4dbb
manuel2005225 <malvarez660@unab.edu.co>
2025-05-05 22:53:52 -0500

    Barra de aceite

--- Assets/SCRIPTS/JUG/LinternaController.cs
+++ Assets/SCRIPTS/JUG/LinternaController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Rendering.Universal;
-
 
 public class LinternaController : MonoBehaviour
 {
@@ -8,6 +8,9 @@ public class LinternaController : MonoBehaviour
     public GameObject LuzLinterna;
 
     public float aceite = 100f;
+    public float aceiteMax = 100f;
+
+    public Image barraAceite; // Arrástralo desde el Inspector (Barra > fill)
 
     void Update()
     {
@@ -29,24 +32,27 @@ public class LinternaController : MonoBehaviour
             }
         }
 
-        // Consumo de aceite basado en la luz activa
+        // Consumo de aceite
         if (LuzArea.activeSelf)
         {
-            aceite -= 0.5f * Time.deltaTime; // Consumo constante por segundo
-            if (aceite <= 0)
-            {
-                aceite = 0;
-                LuzArea.SetActive(false); // Apagar luz si se queda sin aceite
-            }
+            aceite -= 0.5f * Time.deltaTime;
         }
         else if (LuzLinterna.activeSelf)
         {
-            aceite -= 1.0f * Time.deltaTime; // Consumo constante por segundo
-            if (aceite <= 0)
-            {
-                aceite = 0;
-                LuzLinterna.SetActive(false); // Apagar luz si se queda sin aceite
-            }
+            aceite -= 1.0f * Time.deltaTime;
+        }
+
+        if (aceite <= 0)
+        {
+            aceite = 0;
+            LuzArea.SetActive(false);
+            LuzLinterna.SetActive(false);
+        }
+
+        // Actualiza la barra
+        if (barraAceite != null)
+        {
+            barraAceite.fillAmount = aceite / aceiteMax;
         }
     }
 }
